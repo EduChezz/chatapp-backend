5ecf42cfc314e6dc6dd2ec2353862a8687f238f4
EduChezz <javieresanchezy@gmail.com>
2026-04-23 23:44:15 -0500

    fix: agregar adaptador de postgres para prisma 7

--- src/config/db.js
+++ src/config/db.js
@@ -1,4 +1,15 @@
+const { Pool } = require('pg');
+const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+
+// Recreamos tu conexión clásica
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // Importante para que Render no bloquee la conexión
+});
+
+// Le pasamos la conexión al adaptador de Prisma 7
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
