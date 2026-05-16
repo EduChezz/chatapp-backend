eaeba070cd0a1724af6072c0aa1c6fcf20cc272c
EduChezz <javieresanchezy@gmail.com>
2026-05-16 11:46:58 -0500

    fix: prisma 5 + cloudinary v1

--- src/config/db.js
+++ src/config/db.js
@@ -2,13 +2,11 @@ const { Pool } = require('pg');
 const { PrismaPg } = require('@prisma/adapter-pg');
 const { PrismaClient } = require('@prisma/client');
 
-// Recreamos tu conexión clásica
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // Importante para que Render no bloquee la conexión
+  ssl: { rejectUnauthorized: false }
 });
 
-// Le pasamos la conexión al adaptador de Prisma 7
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
